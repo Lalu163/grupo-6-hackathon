3cb5440d73c8e578e511c480be60a4bf5f5fb451
fernando <fernandodendara@gmail.com>
2022-02-17 04:12:08 -0500

    Cardtlist structure

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,11 +1,15 @@
 import { get } from "../../Helper/get"
 import { useEffect } from "react"
+import { Card } from "../Card/Card"
+
 export const CardList = () => {
-    const [items, setItems] 
+    const [items, setItems] = useState([])
     useEffect(() => {
-        get().then(data)
+        get().then((data) => setItems(data))
     },[])
     return (
-
+        <div>
+            
+        </div>
     )
 }
