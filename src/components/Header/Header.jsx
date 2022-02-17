1d366d7aa5973631f8307f60a7bc0b7eb188cdc0
Laura Contreras <m.laura.cp89@gmail.com>
2022-02-17 15:07:15 +0100

    navegador hecho

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import HelPet from "../../assets/logo/HelPet.png"
+import menu from "../../assets/icons/menu.png"
 import styles from "./Header.module.css"
+import Navbar from "../Navbar/Navbar"
+
 function Header(){
+   const [show, setShow]=useState(false)
     return(
         <div className="header">
+            {show?<Navbar/>:null}
             <div className={styles.header}>
-            <img src={HelPet} className="image-logo"></img>
+             <img src={HelPet} className={styles.logo}></img>
+             <img src={menu} className={styles.menu} onClick={()=>setShow(!show)}></img>
             </div>
+            
         </div>
     );
 }
