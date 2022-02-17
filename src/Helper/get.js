d6c022549f15fcc7ad6be34b29ddb8cf95e5219b
fernando <fernandodendara@gmail.com>
2022-02-17 03:52:45 -0500

    json structure

---
+++ src/Helper/get.js
@@ -0,0 +1,6 @@
+import data from "./data.json"
+
+export const get = async () => {
+    const items = data
+    return items
+}
---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,11 @@
+import { get } from "../../Helper/get"
+import { useEffect } from "react"
+export const CardList = () => {
+    const [items, setItems] 
+    useEffect(() => {
+        get().then(data)
+    },[])
+    return (
+
+    )
+}
