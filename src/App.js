e7d440cbdb2699dc654cd592aee6f0061cfe3e70
Laura Contreras <m.laura.cp89@gmail.com>
2022-02-17 11:48:55 +0100

    cambios added header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
+
 import Header from "./components/Header/Header"
-=======
-import { Navbar } from './components/Navbar/Navbar';
->>>>>>> 68af3974d5854d11e2f4e2a896dab7dd0aa9cd60
+
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Header/>
-=======
-      <h1>Aquícolocaremos todos los componentes de la web</h1>
-      <Navbar/>
->>>>>>> 68af3974d5854d11e2f4e2a896dab7dd0aa9cd60
-    </div>
+
   );
 }
 
